37fab0af0e5a9a0f99386a7f200fe2a904c2d50b
killertommi <tommi.2525@hotmail.com>
2018-02-01 14:28:15 +0200

    valmis angular6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { RoutesComponent } from './routes/routes.component';
-import { SetupComponent } from './setup/setup.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
+import {AppComponent} from './app.component';
+import {RoutesComponent} from './routes/routes.component';
+import {SetupComponent} from './setup/setup.component';
+import {TopBarComponent} from './top-bar/top-bar.component';
 import {HttpClientModule} from '@angular/common/http';
 import {DigitransitService} from './services/digitransit.service';
 import {FormsModule} from '@angular/forms';
 import {AgmCoreModule} from '@agm/core';
-import { RouteDirectionPipe } from './pipes/route-direction.pipe';
-import { PiippuPipe } from './pipes/piippu.pipe';
- // generoidut komponentit importataan automaattisesti
+import {RouteDirectionPipe} from './pipes/route-direction.pipe';
+import {PiippuPipe} from './pipes/piippu.pipe';
+import {MatExpansionModule, MatIconModule, MatListModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+// generoidut komponentit importataan automaattisesti
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RoutesComponent,
-    SetupComponent,
-    TopBarComponent,
-    RouteDirectionPipe,
-    PiippuPipe  // declaraatiot (julistukset) automaattisesti generoinnin yhteydessä
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-      HttpClientModule,
-      FormsModule,
-      AgmCoreModule.forRoot({
-          apiKey: 'AIzaSyC0nJCFe4JQqSuOP0BsYyBsTYXXzLZsDUM'  // GOOGLE MAPS API KEY
-      })
-  ],
-  providers: [DigitransitService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        RoutesComponent,
+        SetupComponent,
+        TopBarComponent,
+        RouteDirectionPipe,
+        PiippuPipe  // declaraatiot (julistukset) automaattisesti generoinnin yhteydessä
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyC0nJCFe4JQqSuOP0BsYyBsTYXXzLZsDUM',
+        }),
+        MatExpansionModule,
+        MatListModule,
+        MatIconModule,
+        FlexLayoutModule
+    ],
+    providers: [DigitransitService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/pipes/route-direction.pipe.ts
+++ src/app/pipes/route-direction.pipe.ts
@@ -7,9 +7,9 @@ export class RouteDirectionPipe implements PipeTransform {
 
   transform(value: any, args?: any): any { // ? tarkoittaa valinnaista parametria
     if (value === 0) {
-      return 'Going to ';
+      return 'Going to ';  // arrow right
     } else {
-      return 'Coming from ';
+      return 'Coming from ';  // arrow left
     }
   }
   // value kertoo mitä tyyppiä palauttaa
